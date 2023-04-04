d5a3ffd997fcd07c6d33a73323f0465eb5a0e183
DaeSungYoun <ydskingdom@gmail.com>
2023-04-05 00:11:42 +0900

    회원가입 컨트롤러 유효성검사 AOP 적용

--- junit-bank-class/src/main/java/com/ydskingdom/bank/handler/CustomExceptionHandler.java
+++ junit-bank-class/src/main/java/com/ydskingdom/bank/handler/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ydskingdom.bank.handler;
 
 import com.ydskingdom.bank.dto.ResponseDto;
 import com.ydskingdom.bank.handler.exception.CustomApiException;
+import com.ydskingdom.bank.handler.exception.CustomValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,5 +18,11 @@ public class CustomExceptionHandler {
     public ResponseEntity<?> apiException(CustomApiException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?> customValidationException(CustomValidationException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
     }
 }
--- junit-bank-class/src/main/java/com/ydskingdom/bank/web/UserController.java
+++ junit-bank-class/src/main/java/com/ydskingdom/bank/web/UserController.java
@@ -8,15 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -27,14 +24,6 @@ public class UserController {
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody @Valid UserReqDto.JoinReqDto joinReqDto, BindingResult bindingResult) {
         UserResDto.JoinResDto joinResDto = userService.회원가입(joinReqDto);
-
-        if (bindingResult.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error : bindingResult.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<>(new ResponseDto<>(-1, "유효성 검사 실패", errorMap), HttpStatus.BAD_REQUEST);
-        }
 
         return new ResponseEntity<>(new ResponseDto<>(1, "회원가입 성공", joinResDto), HttpStatus.CREATED);
 
