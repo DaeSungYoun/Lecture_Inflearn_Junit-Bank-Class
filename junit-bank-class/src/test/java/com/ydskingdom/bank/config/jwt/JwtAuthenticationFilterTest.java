8080063b30ed6543bc642c12efc848f9d8f2f03c
DaeSungYoun <ydskingdom@gmail.com>
2023-04-19 23:54:59 +0900

    unsuccessfulAuthentication 테스트

--- junit-bank-class/src/test/java/com/ydskingdom/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ junit-bank-class/src/test/java/com/ydskingdom/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -13,11 +13,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import javax.transaction.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class JwtAuthenticationFilterTest extends DummyObject {
@@ -61,5 +64,29 @@ class JwtAuthenticationFilterTest extends DummyObject {
         assertNotNull(jwtToken);
         assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
         resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
+    }
+
+    @Test
+    void unsuccessfulAuthentication_test() throws Exception {
+        //given
+        UserReqDto.LoginReqDto loginReqDto = new UserReqDto.LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("123");
+
+        String requestBody = objectMapper.writeValueAsString(loginReqDto);
+        System.out.println("requestBody = " + requestBody);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/api/login")
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("responseBody = " + responseBody);
+        System.out.println("jwtToken = " + jwtToken);
+
+        //then
+        resultActions.andExpect(status().isUnauthorized());
     }
 }
