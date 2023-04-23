ded48565ff8a76ebce7ab0cd1b96e8a097c8c08e
DaeSungYoun <ydskingdom@gmail.com>
2023-04-24 00:52:38 +0900

    본인계좌목록보고 컨트롤러 만들기

--- junit-bank-class/src/main/java/com/ydskingdom/bank/web/AccountController.java
+++ junit-bank-class/src/main/java/com/ydskingdom/bank/web/AccountController.java
@@ -2,6 +2,7 @@ package com.ydskingdom.bank.web;
 
 import com.ydskingdom.bank.config.auth.LoginUser;
 import com.ydskingdom.bank.dto.ResponseDto;
+import com.ydskingdom.bank.dto.account.AccountListResDto;
 import com.ydskingdom.bank.dto.account.AccountReqDto;
 import com.ydskingdom.bank.dto.account.AccountResDto;
 import com.ydskingdom.bank.service.AccountService;
@@ -10,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,6 +22,12 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser) {
+        AccountListResDto accountListResDto = accountService.accsountListByUserId(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌목록보기_유저별 성공", accountListResDto), HttpStatus.OK);
+    }
+
     @PostMapping("/s/account")
     public ResponseEntity<?> saveAccount(
             @RequestBody @Valid AccountReqDto accountReqDto,
@@ -31,6 +35,6 @@ public class AccountController {
             @AuthenticationPrincipal LoginUser loginUser) {
         AccountResDto accountResDto = accountService.accountSave(accountReqDto, loginUser.getUser().getId());
 
-        return new ResponseEntity<>(new ResponseDto<>(1, "", accountResDto), HttpStatus.CREATED);
+        return new ResponseEntity<>(new ResponseDto<>(1, "계좌등록 성공", accountResDto), HttpStatus.CREATED);
     }
 }
